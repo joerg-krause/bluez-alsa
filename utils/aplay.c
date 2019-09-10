586fc87b1699c0eeb10c3e282a4f98d9ff669c54
Jörg Krause <joerg.krause@embedded.rocks>
2019-09-10 17:50:02 +0200

    Exit workers on SIGINT

--- utils/aplay.c
+++ utils/aplay.c
@@ -1003,5 +1003,12 @@ usage:
 
 	}
 
+	debug("Exit, stopping %d workers", workers_count);
+	for (i = workers_count; i > 0; i--) {
+		struct pcm_worker *worker = &workers[i - 1];
+		pthread_cancel(worker->thread);
+		pthread_join(worker->thread, NULL);
+	}
+
 	return EXIT_SUCCESS;
 }
